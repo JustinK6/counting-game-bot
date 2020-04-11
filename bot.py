624e4d305a077cb4216e90684e432d0bbc390899
ORaNgCHiKeN <justink6@illinois.edu>
2020-04-10 23:14:56 -0500

    Fixed indentation

--- bot.py
+++ bot.py
@@ -7,18 +7,18 @@ client = commands.Bot(command_prefix = '$')
 
 @client.event
 async def on_ready():
-    await client.change_presence(status = discord.Status.online, activity = discord.Game('WTF'))
+  await client.change_presence(status = discord.Status.online, activity = discord.Game('WTF'))
 
 @client.command()
 async def load(ctx, extension):
-    client.load_extension(f'cogs.{extension}')
+  client.load_extension(f'cogs.{extension}')
     
 @client.command()
 async def unload(ctx, extension):
-    client.unload_extension(f'cogs.{extension}')
+  client.unload_extension(f'cogs.{extension}')
 
 for filename in os.listdir('./cogs'):
-    if (filename.endswith('.py')):
-        client.load_extension(f'cogs.{filename[:-3]}')
+  if (filename.endswith('.py')):
+    client.load_extension(f'cogs.{filename[:-3]}')
 
 client.run('Token here')
--- cogs/utils.py
+++ cogs/utils.py
@@ -4,49 +4,49 @@ from discord.ext import commands
 from discord.ext.commands import has_permissions, CheckFailure
 
 class Utils(commands.Cog):
-    def __init__(self, client):
-        self.client = client
+  def __init__(self, client):
+    self.client = client
 
-    #Events
-    @commands.Cog.listener()
-    async def on_ready(self):
-        print('Bot is ready.')
+  #Events
+  @commands.Cog.listener()
+  async def on_ready(self):
+    print('Bot is ready.')
 
-    @commands.Cog.listener()
-    async def on_member_join(self, member):
-        print(f'{member} has joined the server.')
+  @commands.Cog.listener()
+  async def on_member_join(self, member):
+    print(f'{member} has joined the server.')
 
-    @commands.Cog.listener()
-    async def on_member_remove(self, member):
-        print(f'{member} has left the server.')
+  @commands.Cog.listener()
+  async def on_member_remove(self, member):
+    print(f'{member} has left the server.')
 
-    #Commands
-    @commands.command()
-    async def ping(self, ctx):
-        await ctx.send(f'Pong! {round(self.client.latency * 1000)}ms')
+  #Commands
+  @commands.command()
+  async def ping(self, ctx):
+    await ctx.send(f'Pong! {round(self.client.latency * 1000)}ms')
 
-    @commands.command()
-    @has_permissions(administrator = True, manage_messages = True, manage_roles = True)
-    async def kick(self, ctx, member : discord.Member, *, reason = None):
-        try:
-            await member.kick(reason = reason)
-            await ctx.send(f'{str(member)} has been kicked.')
-        except Exception as error:
-            await ctx.send(f"Can't kick {str(member)} : {error}")
+  @commands.command()
+  @has_permissions(administrator = True, manage_messages = True, manage_roles = True)
+  async def kick(self, ctx, member : discord.Member, *, reason = None):
+    try:
+      await member.kick(reason = reason)
+      await ctx.send(f'{str(member)} has been kicked.')
+    except Exception as error:
+      await ctx.send(f"Can't kick {str(member)} : {error}")
 
-    @commands.command()
-    @has_permissions(administrator = True, manage_messages = True, manage_roles = True)
-    async def ban(self, ctx, member : discord.Member, *, reason = None):
-        try:
-            await member.ban(reason = reason)
-            await ctx.send(f'{str(member)} has been banned.')
-        except Exception as error:
-            await ctx.send(f"Can't ban {str(member)} : {error}")
+  @commands.command()
+  @has_permissions(administrator = True, manage_messages = True, manage_roles = True)
+  async def ban(self, ctx, member : discord.Member, *, reason = None):
+    try:
+      await member.ban(reason = reason)
+      await ctx.send(f'{str(member)} has been banned.')
+    except Exception as error:
+      await ctx.send(f"Can't ban {str(member)} : {error}")
 
-    @commands.command()
-    async def clear(self, ctx, amount = 1):
-        await ctx.channel.purge(limit = amount + 1)
-        print(amount)
+  @commands.command()
+  async def clear(self, ctx, amount = 1):
+    await ctx.channel.purge(limit = amount + 1)
+    print(amount)
 
 def setup(client):
-    client.add_cog(Utils(client))
+  client.add_cog(Utils(client))
